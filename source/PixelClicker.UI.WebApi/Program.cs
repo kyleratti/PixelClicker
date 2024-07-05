e3f3d48f9acd197c16c3d5bb75bf31d80c36701c
Kyle Ratti <kyleratti@users.noreply.github.com>
2024-07-05 11:53:01 -0400

    Initial commit

---
+++ source/PixelClicker.UI.WebApi/Program.cs
@@ -0,0 +1,33 @@
+using PixelClicker.UI.WebApi.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<PixelHub>("/hub");
+
+app.Run();
